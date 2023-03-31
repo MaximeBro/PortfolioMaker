3036db12db5867a0cf34d75f503ade0f780a3ea8
MaximeBro <maximebrochard.pro@gmail.com>
2023-03-31 14:45:05 +0200

    Ajout des classes BD et Auteur + ajout des CGU.

---
+++ php/Auteur.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+class Auteur {
+	private $idAuteur;
+	private $nom;
+	private $prenom;
+	private $email;
+	private $mdp;
+	private $pdp;
+
+	public function __construct($i, $name="Jean", $lastName="Magie", $email="exemple@gmail.com", $password="encrypted", $image="base64") {
+		$this->idAuteur = $i;
+		$this->nom = $name;
+		$this->prenom = $lastName;
+		$this->email = $email;
+		$this->mdp = $password;
+		$this->pdp = $image;
+	}
+
+	public function getId() { return $this->idAuteur; }
+	public function getNom() { return $this->nom;}
+	public function getPrenom() { return $this->prenom; }
+	public function getEmail() { return $this->email; }
+	public function getPassword() { return $this->mdp; }
+	public function getImage() { return $this->pdp; }
+
+	public function __toString() {
+		$res = "id:".$this->idAuteur."\n";
+		$res = $res ."nom:".$this->nom."\n";
+		$res = $res ."prénom:".$this->prenom."\n";
+		$res = $res ."email:".$this->email."\n";
+		$res = $res ."mdp:".$this->mdp."\n";
+		$res = $res ."pdp:".$this->image."\n";
+		$res = $res ."<br/>";
+		return $res;
+
+	}
+}
+
+?>
---
+++ php/DB.inc.php
@@ -0,0 +1,201 @@
+<?php
+
+require 'Auteur.inc.php';
+
+class DB {
+	private static $instance = null; //mémorisation de l'instance de DB pour appliquer le pattern Singleton
+	private $connect=null; //connexion PDO à la base
+
+	/************************************************************************/
+	//	Constructeur gerant  la connexion à la base via PDO
+	//	NB : il est non utilisable a l'exterieur de la classe DB
+	/************************************************************************/
+	private function __construct() {
+		// Connexion à la base de données
+		$connStr = 'pgsql:host=woody port=5432 dbname=la212847';
+		try {
+			// Connexion à la base
+			$this->connect = new PDO($connStr, 'la212847', 'aymeric');
+
+			// Configuration facultative de la connexion
+			$this->connect->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+			$this->connect->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $e) {
+			echo "problème de connexion :".$e->getMessage();
+			return null;
+		}
+	}
+
+	/************************************************************************/
+	//	Methode permettant d'obtenir un objet instance de DB
+	//	NB : cet objet est unique pour l'exécution d'un même script PHP
+	//	NB2: c'est une methode de classe.
+	/************************************************************************/
+	public static function getInstance() {
+		if (is_null(self::$instance)) {
+			try {
+				self::$instance = new DB();
+			}
+			catch (PDOException $e) {
+				echo $e;
+			}
+		} //fin IF
+		$obj = self::$instance;
+
+		if (($obj->connect) == null) {
+			self::$instance=null;
+		}
+		return self::$instance;
+	} //fin getInstance
+
+	/************************************************************************/
+	//	Methode permettant de fermer la connexion a la base de données
+	/************************************************************************/
+	public function close() {
+		$this->connect = null;
+	}
+
+	/************************************************************************/
+	//	Methode uniquement utilisable dans les méthodes de la class DB
+	//	permettant d'exécuter n'importe quelle requête SQL
+	//	et renvoyant en résultat les tuples renvoyés par la requête
+	//	sous forme d'un tableau d'objets
+	//	param1 : texte de la requête à exécuter (éventuellement paramétrée)
+	//	param2 : tableau des valeurs permettant d'instancier les paramètres de la requête
+	//	NB : si la requête n'est pas paramétrée alors ce paramètre doit valoir null.
+	//	param3 : nom de la classe devant être utilisée pour créer les objets qui vont
+	//	représenter les différents tuples.
+	//	NB : cette classe doit avoir des attributs qui portent le même que les attributs
+	//	de la requête exécutée.
+	//	ATTENTION : il doit y avoir autant de ? dans le texte de la requête
+	//	que d'éléments dans le tableau passé en second paramètre.
+	//	NB : si la requête ne renvoie aucun tuple alors la fonction renvoie un tableau vide
+	/************************************************************************/
+	private function execQuery($requete,$tparam,$nomClasse) {
+		//on prépare la requête
+		$stmt = $this->connect->prepare($requete);
+		//on indique que l'on va récupére les tuples sous forme d'objets instance de Client
+		$stmt->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, $nomClasse);
+		//on exécute la requête
+		if ($tparam != null) {
+			$stmt->execute($tparam);
+		}
+		else {
+			$stmt->execute();
+		}
+		//récupération du résultat de la requête sous forme d'un tableau d'objets
+		$tab = array();
+		$tuple = $stmt->fetch(); //on récupère le premier tuple sous forme d'objet
+		if ($tuple) {
+			//au moins un tuple a été renvoyé
+			while ($tuple != false) {
+				$tab[]=$tuple; //on ajoute l'objet en fin de tableau
+				$tuple = $stmt->fetch(); //on récupère un tuple sous la forme
+				//d'un objet instance de la classe $nomClasse
+			} //fin du while
+		}
+		return $tab;
+	}
+
+	/************************************************************************/
+	//	Methode utilisable uniquement dans les méthodes de la classe DB
+	//	permettant d'exécuter n'importe quel ordre SQL (update, delete ou insert)
+	//	autre qu'une requête.
+	//	Résultat : nombre de tuples affectés par l'exécution de l'ordre SQL
+	//	param1 : texte de l'ordre SQL à exécuter (éventuellement paramétré)
+	//	param2 : tableau des valeurs permettant d'instancier les paramètres de l'ordre SQL
+	//	ATTENTION : il doit y avoir autant de ? dans le texte de la requête
+	//	que d'éléments dans le tableau passé en second paramètre.
+	/************************************************************************/
+	private function execMaj($ordreSQL,$tparam) {
+		$stmt = $this->connect->prepare($ordreSQL);
+		$res = $stmt->execute($tparam); //execution de l'ordre SQL
+		return $stmt->rowCount();
+	}
+
+	/*************************************************************************
+	 * Fonctions qui peuvent être utilisées dans les scripts PHP
+	 *************************************************************************/
+
+	public function getAuteurs() {
+		$requete = 'select * from auteur orber by idauteur';
+		return $this->execQuery($requete, null, 'Auteur');
+	}
+
+	public function insertAuteur($idAuteur, $nom, $prenom, $email, $mdp, $image) {
+		$requete = 'insert into client values(?,?,?,?,?,?)';
+		$tparam = array($idAuteur, $nom, $prenom, $email, $mdp, $image);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function updateAuteur($idAuteur, $nom, $prenom, $email, $mdp, $image) {
+		$requete = 'update auteur set nom = ? , prenom = ? , email = ? , mdp = ? , image = ? where idauteur = ?';
+		$tparam = array($nom, $prenom, $email, $mdp, $image, $idAuteur);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function deleteAuteur($idAuteur) {
+		$requete = 'delete from auteur where idauteur = ?';
+		$tparam = array($idAuteur);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	/*
+
+	public function getClientsAdr($adr) {
+		$requete = 'select * from client where ville = ?';
+		return $this->execQuery($requete,array($adr),'Client');
+	}
+
+	public function getClient($idcli) {
+		$requete = 'select * from client where ncli = ?';
+		return $this->execQuery($requete,array($idcli),'Client');
+	}
+
+	public function insertClient($idcli,$nom,$adr) {
+		$requete = 'insert into client values(?,?,?)';
+		$tparam = array($idcli,$nom,$adr);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function updateClient($idcli,$nom,$adr) {
+		$requete = 'update client set nom = ? , ville = ? where ncli = ?';
+		$tparam = array($nom,$adr,$idcli);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function updateAdrClient($idcli,$adr) {
+		$requete = 'update client set ville = ? where ncli = ?';
+		$tparam = array($adr,$idcli);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function deleteClient($idcli) {
+		$requete = 'delete from client where ncli = ?';
+		$tparam = array($idcli);
+		return $this->execMaj($requete,$tparam);
+	}
+
+
+	public function deleteProduit($np) {
+		$requete = 'delete from produit where np = ?';
+		$tparam = array($np);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function insertProduit($np,$lib,$coul,$qs) {
+		$requete = 'insert into produit values(?,?,?,?)';
+		$tparam = array($np,$lib,$coul,$qs);
+		return $this->execMaj($requete,$tparam);
+	}
+
+	public function updateProduit($np,$lib,$couleur,$qs) {
+		$requete = 'update produit set lib = ? , coul = ? , qs = ? where np = ?';
+		$tparam = array($lib,$couleur,$qs,$np);
+		return $this->execMaj($requete,$tparam);
+	}*/
+
+} //fin classe DB
+
+?>
